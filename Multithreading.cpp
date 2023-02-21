57ee6fc6ac0ef429a526047e0338283390f0eeca
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-21 13:54:35 +0530

    Example thread
    threaded created must be join or it will thorw an error

--- Multithreading.cpp
+++ Multithreading.cpp
@@ -10,6 +10,23 @@ void ARandomFunction ()
     printf("A Random hlw world from a random function !! : %d \n",std::this_thread::get_id());
 }
 
+void TestFunction ()
+{
+    printf("called from test function \n");
+}
+void ThreadFun_A ()
+{
+    
+    printf("called from thread A \n");
+    std::thread thread_C(TestFunction);
+    thread_C.join();
+}
+void ThreadFun_B ()
+{
+    printf("called from thread B \n");
+    
+}
+
 class ARandomClass
 {
 public:
@@ -24,26 +41,29 @@ public:
 int main()
 {
     // creating a new thread    #1
-    std::thread thread1 (ARandomFunction);
+   // std::thread thread1 (ARandomFunction);
 
     ARandomClass ARandomObject ;
 
     // creating a new thread    #2
-    std::thread thread2 (ARandomObject);
+    //std::thread thread2 (ARandomObject);
 
-    //here we are going to create more thread i am stop counting
-    std::thread thread3 ([]
+    //here we are going to create more thread i am stop counting    #3
+    /*std::thread thread3 ([]
     {
         printf( "A Random hlw world from inside the thread : %d\n",std::this_thread::get_id());
         
-    });
-    
-    // forcing the main thread to execute this thread
-    thread1.join();
-    thread2.join();
-    thread3.join();
+    });*/
 
-    printf("A Random hlw world from  main  \n");
+    std:std::thread thread_A(ThreadFun_A);
+    std::thread thread_B (ThreadFun_B);
+    
+
+    thread_A.join();
+    thread_B.join();
+
+    //main thread
+    //printf("A Random hlw world from  main : %d \n" ,std::this_thread::get_id());
     
     
 }
